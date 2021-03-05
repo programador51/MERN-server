c54c1fc84797a7c06550e30753675cbf8a5c5d77
programador51 <gskitcat@gmail.com>
2021-03-05 09:48:26 -0600

    CORS

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ const app = express();
 /* Habilitar CORS */
 app.use(cors());
 
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-next();
-});
-
 /* Conectar la BD */
 conection.conectionDB();
 
 
 /* Habilitar JSON desde las peticiones front */
 app.use(express.json({extended:true}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://eager-elion-995c91.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 
 /* Heroku waits this variable name on the deployment 
 Server and client cant have the same port
